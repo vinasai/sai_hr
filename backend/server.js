4b27dbb0bf62a733ae0047b7082dd04b206adb5f
sathu0622 <sathushan622@gmail.com>
2025-02-25 01:52:18 +0530

    backend_Mongodb_changes

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ connectToDatabase();
 
 // Configure cors
 const corsOptions = {
-    origin: "http://localhost:5173", 
+    origin: "http://localhost:5174", 
     methods: "GET,POST,PUT,DELETE",
     credentials: true, 
   };
